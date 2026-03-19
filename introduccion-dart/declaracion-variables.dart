27b150ccf850ac71d8ebc1fb1088fb023cc817e5
gmcruzgonzabay <gmcruzgonzabay@gmail.com>
2026-03-18 20:08:58 -0500

    Actualizacion de archivos

---
+++ introduccion-dart/declaracion-variables.dart
@@ -0,0 +1,20 @@
+void main() {
+  var nombre = "Tony"; //String"
+  const double pi= 3.1416;
+  var apellido = "Start";
+  var edad = 20; //entero
+  var estado = true; //valor booleano
+ // var sueldo = 800.2; //double float
+  
+  int sueldo = 1200;
+  
+
+  //declaro como var una variable
+  //no puedo cambiar el tipo de dato
+
+  //Null Safety
+  //Comentarios
+
+  print("Hola Mundo!");
+  print("Bienvenido a la clase $nombre - $apellido");
+}
